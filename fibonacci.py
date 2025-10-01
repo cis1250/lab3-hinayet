425168a9af8a243389868fd3600ba740821596b4
Hassan Inayet <hinayet@uoguelph.ca>
2025-10-01 12:47:54 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,17 @@
 
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
-positive_integer = int(input("How many terms? ")).
-if num == 1-5:
-    print("")
+positive_integer = int(input("User Input: ")).
+if num == 1
+    print("0")
+else if num == 2
+print("0 1")
+else if num == 3
+print("0 1 1")
+else if num == 4
+print("0 1 1 2")
+else if num == 5
+print("0 1 1 2 3")
+else
+print(Error! input valid integer.)
 # Use a for loop to print the Fibonacci sequence up to that many terms.
